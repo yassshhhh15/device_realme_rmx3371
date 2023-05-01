70929f3673580b0d720c98ac54e6fb2d18aa27e8
Mashopy <eliasgheeraert@gmail.com>
2023-05-01 12:35:17 +0200

    sm8250-common: init: Import OPlus wifi firmware update functions from init.oplus.wifi.sh
    After a manual verification, it seems that OPlus really did updated the wifi firmware at some point
    since the md5sum for regdb.bin doesn't match anymore.
    
    Change-Id: I475742f6a9d606a370005f9c6ee6ba82121249ae

--- init/init.oplus.sh
+++ init/init.oplus.sh
@@ -8,3 +8,27 @@
 if grep -q simcardnum.doublesim=1 /proc/cmdline; then
     setprop vendor.radio.multisim.config dsds
 fi
+
+
+# Wifi
+persistbdf=`md5sum /mnt/vendor/persist/bdwlan.elf |cut -d" " -f1`
+persistRegdb=`md5sum /odm/etc/wifi/regdb.bin |cut -d" " -f1`
+vendorbdf=`md5sum /odm/etc/wifi/bdwlan.elf |cut -d" " -f1`
+vendorRegdb=`md5sum /mnt/vendor/persist/regdb.bin |cut -d" " -f1`
+
+if [ x"$vendorbdf" != x"$persistbdf" ]; then
+    cp /odm/etc/wifi/bdwlan.elf /mnt/vendor/persist/bdwlan.elf
+    sync
+    echo "bdf check"
+fi
+
+if [ x"$vendorRegdb" != x"$persistRegdb" ]; then
+    cp /odm/etc/wifi/regdb.bin /mnt/vendor/persist/regdb.bin
+    sync
+    echo "regdb check"
+fi
+
+chmod 666 /mnt/vendor/persist/bdwlan.elf
+chmod 666 /mnt/vendor/persist/regdb.bin
+chown system:wifi /mnt/vendor/persist/bdwlan.elf
+chown system:wifi /mnt/vendor/persist/regdb.bin
