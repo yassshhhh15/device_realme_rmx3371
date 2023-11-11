700a5fce6bccc37b7d127ff710357882c2db6f80
Mashopy <eliasgheeraert@gmail.com>
2023-11-11 11:42:31 +0100

    sm8250-common: := > += for vintf manifest inclusion
    Change-Id: I4b3db14c11333fdc64f12f2eb9123c45d208cf5b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -74,8 +74,8 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/device_framework_matrix.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):init_realme_kona
