617c16643694367e49793c2739c219fbc1ec9370
aswin7469 <aswinas7469@gmail.com>
2024-02-09 13:04:58 +0530

    sm8250-common:Import SpatialAudio
    *SpatialAudio blobs from cheetah-user 13 TQ2A.230405.003.E1 9802792 release-keys
    Change-Id: a421a3d97bd4694bb746b614460433c24d663d6f
    Signed-off-by:bibindasb <bibindasappu1@gmail.com>

--- common.mk
+++ common.mk
@@ -65,6 +65,21 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.config.vc_call_vol_steps=9 \
     persist.vendor.audio_hal.dsp_bit_width_enforce_mode=24
 
+# Spatial Audio
+PRODUCT_PACKAGES += \
+    libspatialaudio \
+    frameworks/native/data/etc/android.hardware.sensor.dynamic.head_tracker.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.dynamic.head_tracker.xml
+
+# Spatial Audio: optimize spatializer effect
+PRODUCT_PROPERTY_OVERRIDES += \
+       audio.spatializer.effect.util_clamp_min=300
+
+# Spatial Audio: declare use of spatial audio
+PRODUCT_PROPERTY_OVERRIDES += \
+       ro.audio.spatializer_enabled=true \
+       ro.audio.spatializer_transaural_enabled_default=false \
+       persist.vendor.audio.spatializer.speaker_enabled=true
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
