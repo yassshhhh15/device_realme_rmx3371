501a7b2e6eede9dc25f461601850215a2553d8e6
Bibin Das B <bibindasappu1@gmail.com>
2024-06-21 19:04:52 +0530

    RMX3371: Update Vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,6 @@
+git clone --depth=1 https://github.com/AOSPA-for-realme-GT-Neo-3T/vendor_realme_rmx3371.git -b uvite vendor/realme/rmx3371
+git clone https://github.com/AOSPA-for-realme-GT-Neo-3T/hardware_oplus.git -b uvite hardware/oplus
+git clone --depth=1 https://github.com/bibindasb/kernel_realme_rmx3371.git -b non-ksu kernel/msm-4.19
 git clone https://github.com/AOSPA/android_hardware_qcom_audio.git -b uvite-865 vendor/qcom/opensource/audio-hal/primary-hal
 git clone https://github.com/AOSPA/android_hardware_qcom_display.git -b uvite-865 hardware/qcom/display
 git clone https://github.com/AOSPA/android_hardware_qcom_gps.git -b uvite-legacy-component hardware/qcom/gps
